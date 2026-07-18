4b21391315cdc75bba8690ef800367104ea18598
Fabio Fischer <fabiolfischer32@gmail.com>
2026-07-17 21:26:32 -0300

    add stageless postgresql environtment

---
+++ infra/database.js
@@ -0,0 +1,23 @@
+import { Client } from "pg";
+import { config } from "dotenv";
+
+async function query(command) {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    database: process.env.POSTGRES_DB,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+  });
+  await client.connect();
+
+  const result = await client.query(command);
+
+  await client.end();
+
+  return result;
+}
+
+export default {
+  query: query,
+};
--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,11 @@
-function status(request, response) {
-  response.status(200).json({ "chave": "valor"})
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  var result = await database.query(" SELECT 'Alive' as result; ");
+
+  console.log(result.rows[0].result);
+
+  response.status(200).json({ chave: "valor" });
 }
 
-export default status
+export default status;
