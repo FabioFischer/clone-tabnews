b08c8d7f94b378cbc875d8833ce6b952d51cc99c
Fabio Fischer <fabiolfischer32@gmail.com>
2026-08-07 21:25:34 -0300

    refactoring `test/orchestrator.js` file

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -5,12 +5,17 @@ async function waitForAllServices() {
 
   async function waitForWebServer() {
     await retry(fetchStatusPage, {
-      retries: 100
+      retries: 100,
+      maxTimeout: 1000,
     });
 
     async function fetchStatusPage() {
       const response = await fetch("http://localhost:3000/api/v1/status");
       const responseBody = await response.json();
+
+      if (responseBody.status !== "200") {
+        throw new Error("Web server is not ready yet");
+      }
     }
   }
 }
